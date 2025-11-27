ec8f4832c1878868d26ed1b2fc58d022f4d1ec34
Marco Cayuela <marco.cayuela@gmail.com>
2025-11-27 14:57:10 +0100

    mm

--- run.sh
+++ run.sh
@@ -3,17 +3,19 @@
 #SBATCH -p gpu
 #SBATCH --time=01:00:00
 #SBATCH -J kolmogorov90
+#SBATCH -o $SCRACTH/logs/kolmogorov90.out
 
 module purge
 module load python/3.11  
 
 source activate fto
+pip install -r requirements.txt
 
-rsync -av $STORE/datasets/projectA $SCRATCH/projectA/
+rsync -av $STORE/datasets $SCRATCH
 
 export DATA_DIR=$SCRATCH/
 export LOG_DIR=$SCRATCH/
 
 python main.py 
-conda deactivate
+source deactivate
 
