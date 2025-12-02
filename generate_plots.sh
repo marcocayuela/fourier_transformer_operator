c2592491c0ea8750b1f5134bb66535df8109f79f
Marco Cayuela <marco.cayuela@gmail.com>
2025-12-02 09:46:35 +0100

    mm

---
+++ generate_plots.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p gpu
+#SBATCH --time=01:00:00
+#SBATCH -J kolmogorov90
+#SBATCH -o /scratch/cayuelam/logs/kolmogorov/%x_%j.out
+
+module purge
+module load python/3.11  
+
+source activate fto
+pip install -r requirements.txt
+
+rsync -av $STORE/data/kolmogorov/ $SCRATCH/data/kolmogorov
+
+export DATA_DIR=$SCRATCH/data/
+export LOG_DIR=$SCRATCH/runs/
+
+python generate_plots.py 
+source deactivate
+
